39e9c349edadc668216fe2a64140cc631100068d
Mozamel Jawad <mozameljawad@gmail.com>
2024-01-15 13:57:53 +0430

    solve rails linter offenses

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::GreetingsController < ApplicationController
   def random
     greeting = Message.order('RANDOM()').first
-    render json: { greeting: greeting.content}
+    render json: { greeting: greeting.content }
   end
 end
